bdae92482f2ca0dca332412b6ce3e96af08641d0
gitlabhq <gitlabhq@dev.cs.m>
2014-05-06 16:12:30 +0200

    Make appie users and give them deploy sshkey

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,13 +6,66 @@ class appie {
 
     exec { "dpkg -i /tmp/appie-1.8.1.deb":
         alias => "install appie",
-        path => [ "/bin/", "/usr/bin/", "/usr/local/sbin", "/usr/sbin", "/sbin" ],
+        path => [ "/bin", "/usr/bin", "/usr/sbin", "/sbin" ],
         require => File["/tmp/appie-1.8.1.deb"]
     }
 
     file { "/etc/sudoers.d/appie_applications":
         source => "puppet:///modules/appie/appie_applications",
-	owner => root,
-	group => root,
+        owner => root,
+        group => root,
+    }
+
+    define app($app) {
+        #exec { "appie app:mkenv $app $name":
+        #    path => [ "/bin", "/usr/bin", "/usr/sbin", "/sbin" ],
+        #}
+
+        $home_dir = "/opt/APPS/$app/$name"
+        $ssh_dir = "$home_dir/.ssh"
+        $user = "app-$app-$name"
+
+        group { $user:
+        }
+        user { $user:
+            ensure => 'present',
+            gid => $user,
+            home => $home_dir,
+            managehome => true,
+            require => Group[$user],
+        }
+
+        file { $ssh_dir:
+            require => User[$user],
+            ensure => directory,
+            owner => $user,
+            group => $user,
+            mode => '0700',
+        }
+
+        # Store SSH keys so we can pull from git.gw20e.com
+        # TODO: this probably isn't secure..
+        file { "${ssh_dir}/id_rsa":
+            require => File[$ssh_dir],
+            owner => $user,
+            group => $user,
+            mode => 600,
+            source => "puppet:///modules/appie/ssh/id_rsa"
+        }
+        file { "${ssh_dir}/id_rsa.pub":
+            require => File[$ssh_dir],
+            owner => $user,
+            group => $user,
+            mode => 600,
+            source => "puppet:///modules/appie/ssh/id_rsa.pub"
+        }
+        file { "${ssh_dir}/known_hosts":
+            require => File[$ssh_dir],
+            owner => $user,
+            group => $user,
+            mode => 600,
+            source => "puppet:///modules/appie/ssh/known_hosts"
+        }
+
     }
 }
