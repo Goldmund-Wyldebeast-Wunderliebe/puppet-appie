65f334fea761397d3e6c21eca5fb89238931f512
Job Ganzevoort <ganzevoort@gw20e.com>
2014-05-06 10:57:31 +0200

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+class appie {
+    file { "/tmp/appie-1.8.1.deb":
+        ensure => file,
+        source => "puppet:///modules/appie/appie-1.8.1.deb",
+    }
+
+    exec { "dpkg -i /tmp/appie-1.8.1.deb":
+        alias => "install appie",
+        path => [ "/bin/", "/usr/bin/", "/usr/local/sbin", "/usr/sbin", "/sbin" ],
+        require => File["/tmp/appie-1.8.1.deb"]
+    }
+
+    file { "/etc/sudoers.d/appie_applications":
+        source => "puppet:///modules/appie/appie_applications",
+    }
+}
