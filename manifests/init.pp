6164fb5001f1116b5c9602f9e59357eb1275d77e
gitlabhq <gitlabhq@dev.cs.m>
2014-05-07 12:46:15 +0200

    Don't git clone, leave that to deploy

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,7 @@ class appie {
             'python-virtualenv', 'python-pip', 'python-dev',
             'python-psycopg2', 'python-sqlite', 'git', 'libxslt-dev',
             'sqlite3', 'gettext',
+            'sudo',
         ]:
         ensure => installed,
     }
@@ -21,16 +22,17 @@ class appie {
         mode => '0755',
     }
 
-    define parent_dir() {
+    define app($envs) {
         file { "/opt/APPS/$name":
             ensure => directory,
             owner => root,
             group => root,
             mode => '0755',
         }
+        appie::appenv { $envs: app => $name }
     }
 
-    define app($app, $source) {
+    define appenv($app) {
         $home_dir = "/opt/APPS/$app/$name"
         $ssh_dir = "$home_dir/.ssh"
         $user = "app-$app-$name"
@@ -47,56 +49,5 @@ class appie {
             shell => '/bin/bash',
         }
 
-        file { $ssh_dir:
-            require => User[$user],
-            ensure => directory,
-            owner => $user,
-            group => $user,
-            mode => '0700',
-        }
-
-        # Store SSH keys so we can pull from git.gw20e.com
-        # TODO: this probably isn't secure..
-        file { "${ssh_dir}/id_rsa":
-            require => File[$ssh_dir],
-            owner => $user,
-            group => $user,
-            mode => 600,
-            source => "puppet:///modules/appie/ssh/id_rsa"
-        }
-        file { "${ssh_dir}/id_rsa.pub":
-            require => File[$ssh_dir],
-            owner => $user,
-            group => $user,
-            mode => 600,
-            source => "puppet:///modules/appie/ssh/id_rsa.pub"
-        }
-        file { "${ssh_dir}/known_hosts":
-            require => File[$ssh_dir],
-            owner => $user,
-            group => $user,
-            mode => 600,
-            source => "puppet:///modules/appie/ssh/known_hosts"
-        }
-        file { "${ssh_dir}/authorized_keys":
-            require => File[$ssh_dir],
-            owner => $user,
-            group => $user,
-            mode => 600,
-            source => "puppet:///modules/appie/ssh/authorized_keys"
-        }
-
-        vcsrepo { "${home_dir}/project":
-            require => [
-                User[$user],
-                File["${ssh_dir}/known_hosts"],
-                File["${ssh_dir}/id_rsa"],
-            ],
-            ensure => present,
-            user => $user,
-            provider => git,
-            source => $source,
-            #revision => $buildout_rev,
-        }
     }
 }
