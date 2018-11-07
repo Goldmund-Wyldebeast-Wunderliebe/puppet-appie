8fdf15831971226e2754e71b9baf144c9c83ce67
Job Ganzevoort <ganzevoort@gw20e.com>
2018-11-07 14:56:27 +0100

    create users

--- manifests/users.pp
+++ manifests/users.pp
@@ -3,8 +3,45 @@ class appie::users (
     user {
         $appie::gone_users:
             ensure => absent;
-        unique($appie::root_users + $appie::users):
-            ensure => 'present';
+    }
+
+    each (unique($appie::root_users + $appie::users)) |$user| {
+	notify { "user $user":; }
+
+        $home_base = "/home"
+        $home_dir = "$home_base/$user"
+        $ssh_dir = "$home_dir/.ssh"
+        $allowed_users = [$user]
+
+        user { $user:
+            ensure  => 'present',
+            uid     => $uid,
+            gid     => $user,
+            home    => $home_dir,
+            shell   => '/bin/bash',
+        }
+        file {
+            $home_dir:
+                require => User[$user],
+                ensure => directory,
+                owner => $user,
+                group => $user,
+                mode => '0755';
+            $ssh_dir:
+                require => [User[$user], File[$home_dir]],
+                ensure => directory,
+                owner => $user,
+                group => $user,
+                mode => '0700';
+            "${ssh_dir}/authorized_keys":
+                require => File[$ssh_dir],
+                owner => $user,
+                group => $user,
+                mode => '0600',
+                content => epp("appie/ssh/authorized_keys.epp", {
+                    'accounts'    => $allowed_users,
+                    'accountinfo' => $appie::accountinfo,
+                });
+        }
     }
 }
-
