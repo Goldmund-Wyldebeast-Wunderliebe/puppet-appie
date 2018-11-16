e7e8884aef2282d6cd4038b17dd383be62bb5035
Job Ganzevoort <ganzevoort@gw20e.com>
2018-11-16 11:45:04 +0100

    Add sudo rule for specific users

--- manifests/users.pp
+++ manifests/users.pp
@@ -6,8 +6,6 @@ class appie::users (
     }
 
     each (unique($appie::root_users + $appie::users)) |$user| {
-	notify { "user $user":; }
-
         $home_base = "/home"
         $home_dir = "$home_base/$user"
         $ssh_dir = "$home_dir/.ssh"
@@ -15,33 +13,36 @@ class appie::users (
 
         user { $user:
             ensure  => 'present',
-            uid     => $uid,
-            gid     => $user,
             home    => $home_dir,
+            managehome => true,
             shell   => '/bin/bash',
         }
         file {
-            $home_dir:
-                require => User[$user],
-                ensure => directory,
-                owner => $user,
-                group => $user,
-                mode => '0755';
             $ssh_dir:
-                require => [User[$user], File[$home_dir]],
-                ensure => directory,
-                owner => $user,
-                group => $user,
-                mode => '0700';
+                require => User[$user],
+                ensure  => directory,
+                owner   => $user,
+                group   => $user,
+                mode    => '0700';
             "${ssh_dir}/authorized_keys":
                 require => File[$ssh_dir],
-                owner => $user,
-                group => $user,
-                mode => '0600',
+                owner   => $user,
+                group   => $user,
+                mode    => '0600',
                 content => epp("appie/ssh/authorized_keys.epp", {
                     'accounts'    => $allowed_users,
                     'accountinfo' => $appie::accountinfo,
                 });
+        }
+    }
+    each ($appie::root_users) |$user| {
+        file {
+            "/etc/sudoers.d/$user":
+                content => "$user ALL=(ALL:ALL) NOPASSWD: ALL\n",
+                require => Package['sudo'],
+                owner => root,
+                group => root,
+                mode => '0440';
         }
     }
 }
